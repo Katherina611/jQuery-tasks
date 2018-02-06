b297a7d1a1122c2246673d6fdf0737b29c95c2df
Katherina611 <katarzynag0609@gmail.com>
2018-02-06 10:36:13 +0100

    add solution

--- 4.ProgressBar/js/app.js
+++ 4.ProgressBar/js/app.js
@@ -1,27 +1,30 @@
 $(()=>{
 
-    console.log('działa');
+    console.log('działa ok');
     let btns=$('button');
     let divs=$('.progress-bar');
     console.log(btns);
     console.log(divs);
+    let con=divs.find('span');
+    console.log(con);
     btns.on('click', (e)=>{
         let $width=$(e.target).data('width');
         let $color=$(e.target).data('color');
         let $id=$(e.target).data('id');
         if($id===1){
-            let $span=divs.children;
+            let $span=divs.find('span').eq(0);
             $span.toggleClass($color);
             $span.css('width',$width);
         }else if ($id===2){
-            let $span=divs.children;
+            let $span=divs.find('span').eq(1);
             $span.toggleClass($color);
             $span.css('width',$width);
         }else if ($id===3) {
-            let $span = divs.children;
+            let $span = divs.find('span').eq(2);
             $span.toggleClass($color);
             $span.css('width', $width);
         }
 
     })
+
 });
